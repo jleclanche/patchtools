6fd6dcf59bc6b5b3a308605352200222eb738554
Jerome Leclanche <adys@azura>
2011-08-27 07:59:15 +0100

    Initial commit

---
+++ bencode.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+The contents of this file are subject to the BitTorrent Open Source License
+Version 1.1 (the License).  You may not copy or use this file, in either
+source code or executable form, except in compliance with the License.  You
+may obtain a copy of the License at http://www.bittorrent.com/license/.
+
+Software distributed under the License is distributed on an AS IS basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
+for the specific language governing rights and limitations under the
+License.
+
+Written by Petru Paler
+Updated by Jerome Leclanche <adys.wh@gmail.com>
+"""
+
+class BencodeException(Exception):
+	pass
+
+
+class Bencached(object):
+	__slots__ = ["bencoded"]
+
+	def __init__(self, s):
+		self.bencoded = s
+
+def encode_bencached(x,r):
+	r.append(x.bencoded)
+
+def encode_int(x, r):
+	r.extend(("i", str(x), "e"))
+
+def encode_bool(x, r):
+	if x:
+		encode_int(1, r)
+	else:
+		encode_int(0, r)
+
+def encode_string(x, r):
+	r.extend((str(len(x)), ":", x))
+
+def encode_list(x, r):
+	r.append("l")
+	for i in x:
+		encode_func[type(i)](i, r)
+	r.append("e")
+
+def encode_dict(x,r):
+	r.append("d")
+	ilist = list(x.items())
+	ilist.sort()
+	for k, v in ilist:
+		r.extend((str(len(k)), ":", k))
+		encode_func[type(v)](v, r)
+	r.append("e")
+
+encode_func = {
+	Bencached: encode_bencached,
+	bool: encode_bool,
+	dict: encode_dict,
+	int: encode_int,
+	#long: encode_int,
+	list: encode_list,
+	set: encode_list,
+	tuple: encode_list,
+	str: encode_string,
+	#unicode: encode_string,
+}
+
+def bencode(x):
+	r = []
+	encode_func[type(x)](x, r)
+	return "".join(r)
+
+
+def decode_int(x, f):
+	f += 1
+	newf = x.index("e", f)
+	n = int(x[f:newf])
+	if x[f] == "-":
+		if x[f + 1] == "0":
+			raise ValueError
+	elif x[f] == "0" and newf != f+1:
+		raise ValueError
+	return (n, newf+1)
+
+def decode_string(x, f):
+	colon = x.index(":", f)
+	n = int(x[f:colon])
+	if x[f] == "0" and colon != f+1:
+		raise ValueError
+	colon += 1
+	return (x[colon:colon+n], colon+n)
+
+def decode_list(x, f):
+	r, f = [], f+1
+	while x[f] != "e":
+		v, f = decode_func[x[f]](x, f)
+		r.append(v)
+	return (r, f + 1)
+
+def decode_dict(x, f):
+	r, f = {}, f+1
+	while x[f] != "e":
+		k, f = decode_string(x, f)
+		r[k], f = decode_func[x[f]](x, f)
+	return (r, f + 1)
+
+decode_func = {
+	"l": decode_list,
+	"d": decode_dict,
+	"i": decode_int,
+	"0": decode_string,
+	"1": decode_string,
+	"2": decode_string,
+	"3": decode_string,
+	"4": decode_string,
+	"5": decode_string,
+	"6": decode_string,
+	"7": decode_string,
+	"8": decode_string,
+	"9": decode_string,
+}
+
+def bdecode(x):
+	try:
+		r, l = decode_func[x[0]](x, 0)
+	except (IndexError, KeyError, ValueError):
+		raise BencodeException("not a valid bencoded string")
+	if l != len(x):
+		raise BencodeException("invalid bencoded value (data after valid prefix)")
+	return r
---
+++ main.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import os
+from argparse import ArgumentParser
+from bencode import decode_dict as parseTorrent
+from cStringIO import StringIO
+from pywow.mfil import MFIL2 as MFIL
+from urllib import urlopen
+from xml.dom.minidom import getDOMImplementation, parseString
+
+LIVE = 1
+PTR  = 2
+LOCAL_BASE = os.path.join(os.environ.get("HOME"), "mpq")
+
+servers = {
+	LIVE: "http://enus.patch.battle.net:1119/patch",
+	PTR:  "http://public-test.patch.battle.net:1119/patch",
+}
+
+class Downloader(object):
+	def __init__(self, *args):
+		arguments = ArgumentParser() # prog="patchdl"
+		arguments.add_argument("-c", "--client", type=int, dest="client", default=LIVE, help="client version (1 for live, 2 for PTR)")
+		arguments.add_argument("-s", "--server", type=int, dest="server", default=LIVE, help="server version (1 for live, 2 for PTR)")
+		arguments.add_argument("--debug", action="store_true", dest="debug", help="enable debug output")
+		arguments.add_argument("--component", type=str, dest="component", default="enUS", help="program component")
+		arguments.add_argument("--mfil", type=str, dest="mfil", help="Force a specific mfil url")
+		arguments.add_argument("--network", type=str, dest="network", default="akamai", help="Content Distribution Network (possible choices are akamai, att, limelight)")
+		arguments.add_argument("--show-avi", action="store_true", dest="avi", help="include .avi files in the output")
+		arguments.add_argument("program", type=str, nargs="?", default="WoW", help="possible choices are WoW, S2, D3")
+		self.args = arguments.parse_args(*args)
+
+	def debug(self, output):
+		if self.args.debug:
+			print(output)
+
+	def exec_(self):
+		program = self.args.program
+		component = self.args.component
+		server = servers[self.args.server]
+		xml = self.getProgramXML(program, component, self.args.client)
+
+		self.debug("xml=%r" % (xml))
+
+		f = urlopen(server, xml)
+
+		response = f.read()
+		if not response:
+			print("No response from %s" % (server))
+			return 1
+
+		for record in parseString(response).getElementsByTagName("record"):
+			serverProgram = record.getAttribute("program")
+			if serverProgram == "Bnet":
+				continue
+
+			print("%s::%s" % (serverProgram, record.getAttribute("component")))
+
+			data = record.firstChild.data.strip()
+			self.debug("data=%r" % (data))
+
+			base, thash, mhash, build = data.split(";")
+			self.debug("base=%r" % (base))
+			build = int(build)
+			baseUrl = self.getBaseUrl(base, program, self.args.network)
+			tfilUrl = baseUrl + "%s-%i-%s.torrent" % (program.lower(), build, thash)
+			if self.args.mfil:
+				mfilUrl = self.args.mfil
+			else:
+				mfilUrl = baseUrl + "%s-%i-%s.mfil" % (program.lower(), build, mhash)
+
+			self.debug("tfilUrl=%r" % (tfilUrl))
+			self.debug("mfilUrl=%r" % (mfilUrl))
+			self.debug("build=%r" % (build))
+
+			d, length = parseTorrent(urlopen(tfilUrl).read(), 0)
+			directDownload = d["direct download"]
+			self.debug("directDownload=%r" % (directDownload))
+			baseDir = directDownload.split("/")[-1]
+
+			mfil = MFIL(urlopen(mfilUrl))
+
+			i = 0
+			for file, fileInfo in mfil["file"].items():
+				path = os.path.join(LOCAL_BASE, program, baseDir, file)
+
+				if os.path.exists(path):
+					continue
+
+				if int(fileInfo["size"]) == 0:
+					# Directory
+					#print("mkdir -p %s" % (path))
+					continue
+
+				if file.endswith(".avi"):
+					if not self.args.avi:
+						continue
+
+				i += 1
+				print("wget %s/%s -O %s &&" % (directDownload, file, path))
+				#print("%s/%s" % (directDownload, file))
+
+			if not i:
+				print("No new files")
+
+			return 0
+
+	def getBaseUrl(self, base, product, preferredServer):
+		dom = parseString(urlopen(base).read())
+		assert dom.documentElement.tagName == "config"
+		for version in dom.getElementsByTagName("version"):
+			if version.getAttribute("product") == product:
+				for server in version.getElementsByTagName("server"):
+					if server.getAttribute("id") == preferredServer:
+						return server.getAttribute("url")
+
+	def getProgramXML(self, program, component, version):
+		dom = getDOMImplementation().createDocument(None, "version", None)
+		dom.documentElement.setAttribute("program", program)
+
+		record = dom.createElement("record")
+		record.setAttribute("program", "Bnet")
+		record.setAttribute("component", "Win")
+		record.setAttribute("version", "1")
+		dom.documentElement.appendChild(record)
+
+		record = dom.createElement("record")
+		record.setAttribute("program", program)
+		record.setAttribute("component", component)
+		record.setAttribute("version", str(version))
+		dom.documentElement.appendChild(record)
+
+		return dom.documentElement.toxml()
+
+def main():
+	import sys
+
+	app = Downloader(sys.argv[1:])
+	exit(app.exec_())
+
+if __name__ == "__main__":
+	main()
